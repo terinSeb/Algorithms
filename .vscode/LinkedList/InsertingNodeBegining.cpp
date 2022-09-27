5eecd1b0ab2d2b615a5419ef49a392d79c47187f
Terin Sebastin <terin01@gmail.com>
2022-09-28 00:33:59 +0100

    Inserting a node into a list testing

---
+++ .vscode/LinkedList/InsertingNodeBegining.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+ 
+ 
+struct Node
+{
+    int data;
+    Node* link;
+};
+// Driver Code
+
+
+
+int main()
+{
+     Node* A;   
+    A = NULL;
+
+    Node* temp = new Node();
+    temp -> data = 2;
+    temp -> link = NULL;
+     cout <<"OutPut1: " ;
+    cout << A;cout <<" " ;
+     cout <<"OutPut2: " ;
+    cout << temp;cout <<" " ;
+     cout <<"OutPut3: " ;
+     cout << temp -> data ;
+
+    return 0;
+}
