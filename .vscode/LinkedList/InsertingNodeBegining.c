2510be3975e15446a04f9bfbff8256931c703a0c
Terin Sebastin <terin01@gmail.com>
2022-10-31 02:35:51 +0000

    Add at Post LL

---
+++ .vscode/LinkedList/InsertingNodeBegining.c
@@ -0,0 +1,18 @@
+// #include <stdio.h>
+//  #include <stdlib.h>
+// struct Node
+// {
+//     int data;
+//     struct Node* link;
+// };
+// // Driver Code
+
+
+
+// int main()
+// {
+//     struct node *head = (struct node *)malloc(sizeof(struct node));
+//     head->data = 25;
+//     head->link = NULL;
+//     return 0;
+// }
